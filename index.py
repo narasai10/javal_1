fe45f77000ebda2146ba6ed692232ee01d6935e5
Gustavinho Programações UAUUUUUUUUUUU <70520011+narasai10@users.noreply.github.com>
2024-02-19 15:54:22 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -4,6 +4,9 @@ import os
 
 sg.theme("DarkAmber")
 
+def_file = 'javal.txt'
+def_inter = 'execute.py'
+
 replaces = {"->": "print", 
             "<-": "input",
             "(se)": "if",
@@ -12,6 +15,9 @@ replaces = {"->": "print",
 
 layout = [
     [sg.Text("Javal V2", font=("Arial", 22, 'bold'))],
+    [sg.Text("Selecione o arquivo .txt:")],
+    [sg.InputText(def_file, key='txt', disabled=False,text_color="black", background_color="white"), sg.Button("Selecionar!", button_color="red")],
+    [sg.InputText(def_inter, key='py', disabled=False,text_color="black", background_color="white"), sg.Button("Selecionar!", button_color="red")],
     [sg.Multiline(key='text', default_text=open("javal.txt").read(), size=(50, 30), auto_refresh=True, text_color="white", selected_text_color=("black"),
                   background_color=("#2b2b33"))],
     [sg.Button("Salvar", key='btn'), sg.Button("Executar!", key="exec")]
@@ -41,9 +47,11 @@ while True:
             os.system('cls')
         except SyntaxError:
            sg.popup_error_with_traceback("Erro na execução :(", "Corrija essa parada ai...")
+        
         #exec(open("execute.py").read())
 
-
+    if event == 'test':
+       print(value['file-txt'],'\n',value['file-python'])
 
 
 window.close()
